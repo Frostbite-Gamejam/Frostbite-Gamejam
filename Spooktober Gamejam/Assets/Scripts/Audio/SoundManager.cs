eb60d49deb3572682ad806576ae64f3a3b90081b
Zain-Gill <60531627+Zain-Gill@users.noreply.github.com>
2020-10-25 15:17:45 -0600

    Finished Basic Audio System

---
+++ Spooktober Gamejam/Assets/Scripts/Audio/SoundManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+public static class SoundManager
+{
+    public static GameObject PlaySoundOneShot(GameObject objectToPlaySoundOn, AudioClip audioClip, AudioMixerGroup audioMixer, bool looping, float spatialBlend)
+    {
+        GameObject soundObject = new GameObject(objectToPlaySoundOn.name + " Sound");
+        soundObject.AddComponent<AudioSource>();
+        soundObject.transform.position = objectToPlaySoundOn.transform.position;
+        soundObject.transform.rotation = objectToPlaySoundOn.transform.rotation;
+
+        AudioSource audioSource = soundObject.GetComponent<AudioSource>();
+
+        audioSource.outputAudioMixerGroup = audioMixer;
+        audioSource.loop = looping;
+        audioSource.spatialBlend = spatialBlend;
+
+        audioSource.PlayOneShot(audioClip);
+        return soundObject;
+    }
+}
