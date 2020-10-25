65805a3a6108fc3a2b36cd4519399539993faa49
Zain-Gill <60531627+Zain-Gill@users.noreply.github.com>
2020-10-24 18:39:30 -0600

    Added the interaction system

---
+++ Spooktober Gamejam/Assets/Scripts/Create.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Create : MonoBehaviour
+{
+    [SerializeField] private GameObject objectToCreate;
+
+    public void CreateObject()
+    {
+        Instantiate(objectToCreate, new Vector3(0, 10, 0), Quaternion.identity);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.GetComponent<MainPlayer>().playerIsInteracting)
+        {
+            CreateObject();
+            other.GetComponent<MainPlayer>().playerIsInteracting = false;
+        }
+    }
+}
---
+++ Spooktober Gamejam/Assets/Scripts/Destroy.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Destroy : MonoBehaviour
+{
+    [SerializeField] private GameObject objectToDestroy;
+
+
+    public void DisableObject()
+    {
+        objectToDestroy.SetActive(false);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.GetComponent<MainPlayer>().playerIsInteracting)
+        {
+            DisableObject();
+            other.GetComponent<MainPlayer>().playerIsInteracting = false;
+        }
+    }
+}
