0112c587b71398601f1f70a2dd9cee395274ba85
Zain-Gill <60531627+Zain-Gill@users.noreply.github.com>
2020-10-25 08:36:43 -0600

    Implemented component requirments to avoid bugs and confusion

--- Spooktober Gamejam/Assets/Scripts/HighlightEffect.cs
+++ Spooktober Gamejam/Assets/Scripts/HighlightEffect.cs
@@ -1,13 +1,25 @@
 ﻿using UnityEngine;
 
 [RequireComponent(typeof(Outline))]
+[RequireComponent(typeof(BoxCollider))]
 public class HighlightEffect : MonoBehaviour
 {
     private Outline outline;
+    private BoxCollider boxCollider;
 
     private void Awake()
     {
         outline = GetComponent<Outline>();
+        boxCollider = GetComponent<BoxCollider>();
+    }
+
+    private void Start()
+    {
+        outline.enabled = false;
+        if (!boxCollider.isTrigger)
+        {
+            Debug.LogError("The box collider component needs to be a trigger in order for this script to function properly");
+        }
     }
 
     private void OnTriggerStay(Collider other)
