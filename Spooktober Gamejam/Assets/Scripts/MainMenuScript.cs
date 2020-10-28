a92fba7ef2bdec7d5d9412ded9970c4dbc5db625
Zain-Gill <60531627+Zain-Gill@users.noreply.github.com>
2020-10-27 21:19:19 -0600

    Play & Quit Buttons work

--- Spooktober Gamejam/Assets/Scripts/MainMenuScript.cs
+++ Spooktober Gamejam/Assets/Scripts/MainMenuScript.cs
@@ -2,29 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuScript : MonoBehaviour
 {
-    [SerializeField] public Button playButton;
-    [SerializeField] public Button quitButton;
-    // Start is called before the first frame update
-    void Start()
-    {
-        Button play = playButton.GetComponent<playButton>();
-        play.onClick.AddListener(PlayGame);
-        Button quit = quitButton.GetComponent<quitButton>();
-        play.onClick.AddListener(QuitGame);
-    }
-
     // Update is called once per frame
     void Update()
     {
-    
-    }    public void PlayGame() {
-        SceneManagement.LoadScene("Main Level", LoadSceneMode.Additive);
+
     }
 
-    public void QuitGame() {
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Main Level");
+    }
+
+    public void QuitGame()
+    {
         Debug.Log("Quit!");
         Application.Quit();
     }
